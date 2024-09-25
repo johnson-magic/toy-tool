ad10222377544371cc4a4b12d8213fbe4bc2808f
magic_zhang <putaoisapig@163.com>
2024-09-25 22:39:07 +0800

    [REFACTOR]: refactor test module

--- tests/test_read.py
+++ tests/test_read.py
@@ -1,17 +1,22 @@
+import os
 import unittest
 import numpy as np
 from PIL import Image
 
 from ttool.read import ReadImage
 
+current_file_path = os.path.abspath(__file__)
+current_directory = os.path.dirname(current_file_path)
+
 
 class TestReadMethods(unittest.TestCase):
 
     def test_ReadImage(self):
-        img_pillow = ReadImage("./data/color.png", "pillow")
+        path_test_data = os.path.join(current_directory, "./data/color.jpg")
+        img_pillow = ReadImage(path_test_data, "pillow")
         self.assertTrue(isinstance(img_pillow, Image.Image))
 
-        img_opencv = ReadImage("./data/color.png", "opencv")
+        img_opencv = ReadImage(path_test_data, "opencv")
         self.assertTrue(isinstance(img_opencv, np.ndarray))
 
 
